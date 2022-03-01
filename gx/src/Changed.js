9319a6a3fd1a34da66f66d941673315a97544397
Mr-1-GX <76249374+Mr-1-GX@users.noreply.github.com>
2022-03-01 12:33:56 +0400

    react-2022

---
+++ gx/src/Changed.js
@@ -0,0 +1,13 @@
+
+
+const Greeting = ({title, subTitle}) => {
+    return (
+      <header>
+        <h1>{title}</h1>
+        <h3>{subTitle}</h3>
+      </header>
+    );
+  };
+
+
+export default Greeting;
---
+++ gx/src/Welcome.js
@@ -0,0 +1,11 @@
+
+const Welcome =({user, age})=>{
+  return (
+      <div>
+        <h3>Welcome, {user}</h3>
+        <h4>yur age is {age}</h4>
+      </div>
+  )
+}
+
+export default Welcome;
